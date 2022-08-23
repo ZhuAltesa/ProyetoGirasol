a25178f21384e726473a61b9ff189c768816124a
Maax <max.sepulvedaa@duocuc.cl>
2022-08-22 20:04:17 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Proyecto Girasol',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
